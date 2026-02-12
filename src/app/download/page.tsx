c4872f17eec32ef0382587ffcb2912ddd2998c45
Edwinsyah Umasugi <claselysubscription@gmail.com>
2026-02-13 02:00:05 +0700

    fix error

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -6,14 +6,14 @@ import { getSetting } from '@/lib/settings';
 export const revalidate = 60;
 
 export default async function DownloadPage() {
-    const macUrl = await getSetting('mac_download_url');
+
     const winUrl = await getSetting('win_download_url');
 
     return (
         <main className="min-h-screen bg-background selection:bg-primary/20">
             <Navbar />
             <div className="pt-20">
-                <Download macUrl={macUrl || undefined} winUrl={winUrl || undefined} />
+                <Download winUrl={winUrl || undefined} />
             </div>
             <Footer />
         </main>
